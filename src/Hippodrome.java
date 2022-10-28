41353653dbf7bc10a64e5b81e8ae9be16610d11c
Seregakras <Seregakras@yandex.ru>
2022-10-28 20:35:43 +0300

    Update game: add methods getWinner and printWinner and optimize other methods and variables.

--- src/Hippodrome.java
+++ src/Hippodrome.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class Hippodrome {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+    private final List<Horse> horses;
+    public static Hippodrome game;
+
+    public Hippodrome(List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Horse> horses = new ArrayList<>();
+        Hippodrome hippodrome = new Hippodrome(horses);
+        game = hippodrome;
+        hippodrome.horses.add(new Horse("Belka", 3, 0));
+        hippodrome.horses.add(new Horse("Strelka", 3, 0));
+        hippodrome.horses.add(new Horse("Snezhok", 3, 0));
+
+        game.run();
+        game.printWinner();
+    }
+
+    public void run() throws InterruptedException {
+        for (int i = 0; i < 50; i++) {
+            move();
+            print();
+            Thread.sleep(1000);
+        }
+    }
+
+    public void move() {
+        if (horses != null) {
+            for (Horse horse : horses) {
+                horse.move();
+            }
+        }
+    }
+
+    public void print() {
+        if (horses != null) {
+            for (Horse horse : horses) {
+                horse.print();
+            }
+            for (int i = 0; i < 5; i++) System.out.println();
+        }
+    }
+
+    public Horse getWinner() {
+        return horses.get(0).getDistance() > horses.get(1).getDistance() ? (horses.get(0).getDistance() > horses.get(2).getDistance() ? horses.get(0) : horses.get(2)) : (horses.get(1).getDistance() > horses.get(2).getDistance() ? horses.get(1) : horses.get(2));
+    }
+
+    public void printWinner() {
+        System.out.println(getWinner().getName() + " is winner!!!");
+    }
 }
--- src/Horse.java
+++ src/Horse.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Horse {
+public class Horse {
+    private final String name;
+    private final double speed;
+    private double distance;
+
+    public Horse(String name, double speed, double distance) {
+        this.name = name;
+        this.speed = speed;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void move() {
+        distance += speed * Math.random();
+    }
+
+    public void print() {
+        for(int i = 0; i < (int)distance; i++) System.out.print(".");
+        System.out.println(name);
+    }
 }
